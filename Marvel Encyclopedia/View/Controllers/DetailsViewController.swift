064c72a205c109724a6da15c4a76a7462be79806
Diogo Filipe Abreu Rodrigues <rodrigues.diogofilipe@gmail.com>
2024-04-19 17:57:41 +0100

    Added the outlets to the controller

--- Marvel Encyclopedia/View/Controllers/DetailsViewController.swift
+++ Marvel Encyclopedia/View/Controllers/DetailsViewController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class DetailsViewController: UIViewController {
 
+
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var desc: UILabel!
+    @IBOutlet weak var resourceSelector: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+ 
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
