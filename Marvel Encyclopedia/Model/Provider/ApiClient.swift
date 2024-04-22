e8ba0ede2e4c075194fc11ab4b14c6bd2c9a24a9
Diogo Filipe Abreu Rodrigues <rodrigues.diogofilipe@gmail.com>
2024-04-22 16:32:12 +0100

    solved merge conflicts

--- Marvel Encyclopedia/Model/Provider/ApiClient.swift
+++ Marvel Encyclopedia/Model/Provider/ApiClient.swift
@@ -21,11 +21,11 @@ class ApiClient {
             if let error = error {
                 print("Error -------> \(error.localizedDescription)")
             }
-           if (200...299).contains((response as! HTTPURLResponse).statusCode) {
-               if let data = data {
-                   let apiResponse: ResponseCharacter = try! JSONDecoder().decode(ResponseCharacter.self, from: data)
-                   complition(apiResponse)
-               }
+            if (200...299).contains((response as! HTTPURLResponse).statusCode) {
+                if let data = data {
+                    let apiResponse: ResponseCharacter = try! JSONDecoder().decode(ResponseCharacter.self, from: data)
+                    complition(apiResponse)
+                }
             }
         }.resume()
     }
@@ -42,15 +42,11 @@ class ApiClient {
                     guard let image: UIImage = UIImage(data: data) else { return }
                     complition(image)
                 }
-             }
+            }
         }.resume()
     }
+
     
-<<<<<<< Updated upstream
-    func fetchComics(ByCharacterId id : Int ) async throws -> ResponseComic? {
-        var apiResponse: ResponseComic? = nil
-        let endPoint: String = "https://gateway.marvel.com/v1/public/characters/\(id)/comics?ts=1&\(publicKey)&\(hash)"
-=======
     func fetchComics(_ endPoint : String) async throws -> ResponseComic?  {
         print(endPoint)
         var apiResponse: ResponseComic? = nil
@@ -58,10 +54,10 @@ class ApiClient {
             print(1)
             let (data, response) = try await URLSession.shared.data(from: url)
             let encoder = JSONEncoder()
-                encoder.outputFormatting = .prettyPrinted
-
-                    print(String(data: data, encoding: .utf8)!)   //print to console
-              
+            encoder.outputFormatting = .prettyPrinted
+            
+            print(String(data: data, encoding: .utf8)!)   //print to console
+            
             if (200...299).contains((response as! HTTPURLResponse).statusCode) {
                 print(2)
                 apiResponse = try JSONDecoder().decode(ResponseComic.self, from: data)
@@ -72,7 +68,7 @@ class ApiClient {
         print(4)
         return apiResponse
     }
-    
+
     func fetchComics(ByCharacterId id : Int ) async throws -> ResponseComic? {
         let endPoint: String = "https://gateway.marvel.com/v1/public/characters/\(id)/comics?ts=1&\(publicKey)&\(hash)"
         return try await fetchComics(endPoint)
@@ -80,7 +76,7 @@ class ApiClient {
     
     func fetchSeries(_ endPoint : String) async throws -> ResponseSeries? {
         var apiResponse: ResponseSeries? = nil
->>>>>>> Stashed changes
+        
         if let url = URL(string: endPoint) {
             let (data, response) = try await URLSession.shared.data(from: url)
             if (200...299).contains((response as! HTTPURLResponse).statusCode) {
@@ -94,4 +90,7 @@ class ApiClient {
         let endPoint: String = "https://gateway.marvel.com/v1/public/characters/\(id)/series?ts=1&\(publicKey)&\(hash)"
         return try await fetchSeries(endPoint)
     }
+    
+    
+    
 }
